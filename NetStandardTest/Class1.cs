c218c480e19641fd5b958a8ccfad9d5be95b5861
MSDN.WhiteKnight <35516665+MSDN-WhiteKnight@users.noreply.github.com>
2019-11-20 08:23:49 +0500

    Add files via upload

--- NetStandardTest/Class1.cs
+++ NetStandardTest/Class1.cs
@@ -1,1 +1,12 @@
+﻿using System;
 
+namespace NetStandardTest
+{
+    public class Class1
+    {
+        public static double CalcSum(double x, double y)
+        {
+            return x + y;
+        }
+    }
+}
