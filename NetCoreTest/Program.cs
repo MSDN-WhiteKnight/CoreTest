46d00415a7222873c5810547f65b37c7ab037d73
MSDN.WhiteKnight <35516665+MSDN-WhiteKnight@users.noreply.github.com>
2019-11-20 08:24:22 +0500

    Add files via upload

--- NetCoreTest/Program.cs
+++ NetCoreTest/Program.cs
@@ -1,1 +1,12 @@
+﻿using System;
 
+namespace NetCoreTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! {0}",NetStandardTest.Class1.CalcSum(1.0,2.0));
+        }
+    }
+}
